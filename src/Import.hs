7bbc0d1ece339398c55166266bbbea2cc7389fb7
Johan Kiviniemi <devel@johan.kiviniemi.name>
2013-07-17 19:35:32 +0300

    Import: Print parse errors

--- src/Import.hs
+++ src/Import.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Control.Monad
 import Data.Attoparsec.Text.Lazy
 import qualified Data.Text as Text
+import qualified Data.Text.Lazy as TextL
 import qualified Data.Text.Lazy.IO as TextL
 import Database.PostgreSQL.Simple
 import System.Environment
@@ -23,8 +24,10 @@ main =
         hPrint stderr f
         let channel = (Text.pack . takeFileName . takeDirectory) f
 
-        Just gram7s <- maybeResult . parse (logGram7s channel <* endOfInput)
-                   <$> TextL.readFile f
+        gram7s <- parseIO (logGram7s channel <* endOfInput) =<< TextL.readFile f
 
         insertGram7s db gram7s
 
+parseIO :: Parser a -> TextL.Text -> IO a
+parseIO p inp = either (ioError . userError) return
+              $ eitherResult (parse p inp)
